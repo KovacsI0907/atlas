e5cdbc167632a801123ba45648f2bdab3e9d6915
Kovács István <kovacsi0907@gmail.com>
2023-01-30 09:12:04 +0100

    Initial

---
+++ src/client/java/kovacsi0907/atlas/AtlasClient.java
@@ -0,0 +1,10 @@
+package kovacsi0907.atlas;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AtlasClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/kovacsi0907/atlas/AtlasDataGenerator.java
@@ -0,0 +1,11 @@
+package kovacsi0907.atlas;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AtlasDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
