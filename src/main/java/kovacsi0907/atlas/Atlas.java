60a09e62cf7d459448406ade1d1e33cd136cdf53
István Kovács <kovacsi0907@gmail.com>
2023-02-09 10:02:10 +0100

    new test for server manager

--- src/main/java/kovacsi0907/atlas/Atlas.java
+++ src/main/java/kovacsi0907/atlas/Atlas.java
@@ -57,7 +57,6 @@ public class Atlas implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		LOGGER.info("newnewtest");
 		LOGGER.info("Hello Fabric world!");
 		ServerNetworkReceiver.registerListeners();
 		Skills.registerSkills();
